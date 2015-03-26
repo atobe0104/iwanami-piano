b7c71d8436747f48eddd040b53d278762bc1aee3
跡部　達也 <pofupofu2@icloud.com>
2015-03-26 21:08:13 +0900

    faviconを設定

--- header.php
+++ header.php
@@ -12,6 +12,9 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<link rel="SHORTCUT ICON" href="<?php echo get_template_directory_uri(); ?>/favicon.ico"> 
+
 	<title><?php wp_title('|','true','right'); ?></title>
 	<link href='http://fonts.googleapis.com/css?family=Playfair+Display:400,400italic' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
